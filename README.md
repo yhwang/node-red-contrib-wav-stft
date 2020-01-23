449bf90c0f0ba32b059bedd424b5750081c6a3aa
Yihong Wang <yh.wang@ibm.com>
2020-01-22 23:22:05 -0800

    Update README
    update README and add two configurable properties:
    - Window Size
    - Stride Size

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # node-red-contrib-wav-stft
-A Node-RED node to decode wav file and perform short-term Fourier transform (STFT)
+A Node-RED node to decode audio in WAV format and perform short-term Fourier
+transform (STFT). It computes the STFT based on the `Window Size` and
+`Stride Size` settings. The audio WAV should come in as Buffer data type
+from previous node. Here are the processing procedures:
+- Slice the whole audio into fix duration segments according to the
+  `Window Size` value. The `Stride Size` decides the interval between
+  adjacent segments.
+- Compute the STFT and frequency bins is 161
+- Insert a size 1 rank as the first rank and last rank
+
+## Installation
+
+### Prerequisite
+For Tensorflow.js on Node.js
+([@tensorflow/tfjs-node](https://www.npmjs.com/package/@tensorflow/tfjs-node)
+or
+[@tensorflow/tfjs-node-gpu](https://www.npmjs.com/package/@tensorflow/tfjs-node-gpu)),
+it depends on Tensorflow shared libraries. Putting Tensorflow.js as the
+dependency of custom Node-RED node may run into a situation that multiple
+custom nodes install multiple `tfjs-node` module as their dependencies. While
+loading multiple Tensorflow shared libraries in the same process, the process
+would abord by hitting protobuf assertion.
+
+Therefore, this module put `@tensorflow/tfjs-node` as peer dependency. You need
+to install it with the Node-RED manully.
+
+Install `@tensorflow/tfjs-node`:
+```
+npm install @tensorflow/tfjs-node
+```
+
+
+### Install this module:
+Once you install the peer dependency, you can install this module:
+```
+npm install node-red-contrib-wav-stft
+```
--- src/index.ts
+++ src/index.ts
@@ -9,53 +9,54 @@ const STRIDE_SIZE = 10; // 10 mS
 /**
  * Represent Node-Red's runtime
  */
-declare class NodeRed {
+type NodeRed = {
   nodes: NodeRedNodes;
-}
+};
 
-declare class NodeRedWire {
+type NodeRedWire = {
   [index: number]: string;
-}
+};
 
-declare class NodeRedWires {
+type NodeRedWires = {
   [index: number]: NodeRedWire;
-}
+};
 
 /**
  * Represent Node-Red's configuration for a custom node
  * For this case, it's the configuration for DeepSpeech
  * custom node
  */
-declare class NodeRedProperties {
+type NodeRedProperties = {
   id: string;
   type: string;
   name: string;
-  modelURL: string;
+  windowSize: number;
+  strideSize: number;
   wires: NodeRedWires;
-}
+};
 
 /**
  * Represent Node-Red's nodes
  */
-declare class NodeRedNodes {
+type NodeRedNodes = {
   // tslint:disable-next-line:no-any
   createNode(node: any, props: NodeRedProperties): void;
   // tslint:disable-next-line:no-any
   registerType(type: string, ctor: any): void;
-}
+};
 
 /**
  * Represent Node-Red's message that passes to a node
  */
-declare class NodeRedMessage {
+type NodeRedMessage = {
   payload: Buffer|string|tf.Tensor;
-}
+};
 
-declare interface StatusOption {
+type StatusOption = {
   fill: 'red' | 'green' | 'yellow' | 'blue' | 'grey';
   shape: 'ring' | 'dot';
   text: string;
-}
+};
 
 /**
  * Based on the window size and stride size to compose a
@@ -80,9 +81,9 @@ function _getStrideBuff(buff: Float32Array, window: number, stride: number)
  * https://en.wikipedia.org/wiki/Short-time_Fourier_transform.
  * @param buff
  */
-function stft(buff: Float32Array): tf.Tensor {
-  const windowSize = SAMPLE_RATE * 0.001 * WINDOW_SIZE;
-  const strideSize = SAMPLE_RATE * 0.001 * STRIDE_SIZE;
+function stft(buff: Float32Array, wSize: number, sSize: number): tf.Tensor {
+  const windowSize = SAMPLE_RATE * 0.001 * wSize;
+  const strideSize = SAMPLE_RATE * 0.001 * sSize;
   const trancate = (buff.length - windowSize ) % strideSize;
   const trancatedBuff = new Float32Array(
       buff.buffer, 0, buff.length - trancate);
@@ -111,6 +112,10 @@ function stft(buff: Float32Array): tf.Tensor {
   });
 }
 
+function _getNumber(val: number, defaultVal: number) {
+  return Number.isInteger(val) ? val : defaultVal;
+}
+
 // Module for a Node-Red custom node
 export = function wav2STFT(RED: NodeRed) {
 
@@ -124,23 +129,28 @@ export = function wav2STFT(RED: NodeRed) {
     id: string;
     type: string;
     name: string;
+    windowSize: number;
+    strideSize: number;
     wires: NodeRedWires;
 
     constructor(config: NodeRedProperties) {
       this.id = config.id;
       this.type = config.type;
       this.name = config.name;
+      this.windowSize = _getNumber(config.windowSize, WINDOW_SIZE);
+      this.strideSize = _getNumber(config.strideSize, STRIDE_SIZE);
       this.wires = config.wires;
-
+      
       RED.nodes.createNode(this, config);
       this.on('input', (msg: NodeRedMessage) => {
         this.handleRequest(msg);
       });
-
+      
       this.on('close', (done: () => void) => {
         this.handleClose(done);
       });
-
+      this.log(`Window Size: ${this.windowSize}`);
+      this.log(`Stride Size: ${this.strideSize}`);
     }
 
     // handle a single request
@@ -154,7 +164,7 @@ export = function wav2STFT(RED: NodeRed) {
         audio = downsampleTo16K(result.channelData[0], result.sampleRate);
       }
 
-      this.send({payload: stft(audio)});
+      this.send({payload: stft(audio, this.windowSize, this.strideSize)});
     }
 
     handleClose(done: () => void) {
